3a271e251fc81b555bf3e179e1adb8d678ff07e8
chaiushas <chaiushas@gmail.com>
2022-09-15 08:55:25 +0300

    added Lithuanian letters

--- main.py
+++ main.py
@@ -10,53 +10,53 @@ class BoardNum:
         print(f"{objektas.num_array[6]}|{objektas.num_array[7]}|{objektas.num_array[8]}\n")
 
     def turn_x(self):
-        input_x = int(input("Kryziuku eile: "))
+        input_x = int(input("Kryžiukų eile: "))
         if not isinstance(objektas.num_array[input_x - 1], int):
-            print("Cia jau yra padetas zenklas!")
+            print("Čia jau yra padėtas ženklas!")
             objektas.game_window()
             objektas.turn_x()
         if input_x > 9 or input_x < 1:
-            print("Neteisingai ivestas ejimas")
+            print("Neteisingai įvestas ėjimas")
             objektas.game_window()
             objektas.turn_x()
         objektas.num_array[input_x - 1] = "x"
 
     def turn_o(self):
-        input_o = int(input("Nuliuku eile: "))
+        input_o = int(input("Nuliukų eilė: "))
         if not isinstance(objektas.num_array[input_o - 1], int):
             print("Cia jau yra padetas zenklas!")
             objektas.game_window()
             objektas.turn_o()
         if input_o > 9 or input_o < 1:
-            print("Neteisingai ivestas ejimas")
+            print("Neteisingai įvestas ėjimas")
             objektas.game_window()
             objektas.turn_o()
         objektas.num_array[input_o - 1] = "o"
 
     def check_game_state(self):
         if objektas.num_array[0] == objektas.num_array[1] and objektas.num_array[1] == objektas.num_array[2]:
-            print(f"Laimejo {objektas.num_array[0]}")
+            print(f"Laimėjo {objektas.num_array[0]}")
             return True
         if objektas.num_array[3] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[5]:
-            print(f"Laimejo {objektas.num_array[3]}")
+            print(f"Laimėjo {objektas.num_array[3]}")
             return True
         if objektas.num_array[6] == objektas.num_array[7] and objektas.num_array[7] == objektas.num_array[8]:
-            print(f"Laimejo {objektas.num_array[6]}")
+            print(f"Laimėjo {objektas.num_array[6]}")
             return True
         if objektas.num_array[0] == objektas.num_array[3] and objektas.num_array[3] == objektas.num_array[6]:
-            print(f"Laimejo {objektas.num_array[0]}")
+            print(f"Laimėjo {objektas.num_array[0]}")
             return True
         if objektas.num_array[1] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[7]:
-            print(f"Laimejo {objektas.num_array[1]}")
+            print(f"Laimėjo {objektas.num_array[1]}")
             return True
         if objektas.num_array[2] == objektas.num_array[5] and objektas.num_array[5] == objektas.num_array[8]:
-            print(f"Laimejo {objektas.num_array[2]}")
+            print(f"Laimėjo {objektas.num_array[2]}")
             return True
         if objektas.num_array[0] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[8]:
-            print(f"Laimejo {objektas.num_array[0]}")
+            print(f"Laimėjo {objektas.num_array[0]}")
             return True
         if objektas.num_array[2] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[6]:
-            print(f"Laimejo {objektas.num_array[2]}")
+            print(f"Laimėjo {objektas.num_array[2]}")
             return True
         if all(isinstance(x, str) for x in objektas.num_array):
             print("Lygiosios!")
@@ -66,7 +66,7 @@ class BoardNum:
         turn = 0
         while True:
             objektas.num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            play_game = int(input("Ar norite zaisti? 1 - Taip, 2 - Ne "))
+            play_game = int(input("Ar norite žaisti? 1 - Taip, 2 - Ne "))
             if play_game == 2:
                 break
             while True:
