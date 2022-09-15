727ab7f52e90d5c790007c746bc8707e49cb8231
chaiushas <chaiushas@gmail.com>
2022-09-15 10:24:11 +0300

    added win counter

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ class BoardNum:
             self.num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def game_window(self):
-        print(f"{objektas.num_array[0]}|{objektas.num_array[1]}|{objektas.num_array[2]}")
+        print(f"{objektas.num_array[6]}|{objektas.num_array[7]}|{objektas.num_array[8]}")
         print(f"{objektas.num_array[3]}|{objektas.num_array[4]}|{objektas.num_array[5]}")
-        print(f"{objektas.num_array[6]}|{objektas.num_array[7]}|{objektas.num_array[8]}\n")
+        print(f"{objektas.num_array[0]}|{objektas.num_array[1]}|{objektas.num_array[2]}\n")
 
     def turn_x(self):
         input_x = int(input("Kryžiukų eile: "))
@@ -36,31 +36,57 @@ class BoardNum:
     def check_game_state(self):
         if objektas.num_array[0] == objektas.num_array[1] and objektas.num_array[1] == objektas.num_array[2]:
             print(f"Laimėjo {objektas.num_array[0]}")
+            if objektas.num_array[0] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[3] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[5]:
             print(f"Laimėjo {objektas.num_array[3]}")
+            if objektas.num_array[3] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[6] == objektas.num_array[7] and objektas.num_array[7] == objektas.num_array[8]:
             print(f"Laimėjo {objektas.num_array[6]}")
+            if objektas.num_array[6] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[0] == objektas.num_array[3] and objektas.num_array[3] == objektas.num_array[6]:
             print(f"Laimėjo {objektas.num_array[0]}")
+            if objektas.num_array[0] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[1] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[7]:
             print(f"Laimėjo {objektas.num_array[1]}")
+            if objektas.num_array[1] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[2] == objektas.num_array[5] and objektas.num_array[5] == objektas.num_array[8]:
             print(f"Laimėjo {objektas.num_array[2]}")
+            if objektas.num_array[2] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[0] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[8]:
             print(f"Laimėjo {objektas.num_array[0]}")
+            if objektas.num_array[0] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if objektas.num_array[2] == objektas.num_array[4] and objektas.num_array[4] == objektas.num_array[6]:
             print(f"Laimėjo {objektas.num_array[2]}")
+            if objektas.num_array[2] == "x":
+                pergales.win_x += 1
+            else: pergales.win_o += 1
             return True
         if all(isinstance(x, str) for x in objektas.num_array):
             print("Lygiosios!")
+            pergales.draw += 1
             return True
+
 
     def game_state(self):  # True = veikia, False = neveikia
         turn = 0
@@ -72,6 +98,10 @@ class BoardNum:
             while True:
                 turn += 1
                 if objektas.check_game_state():
+                    objektas.game_window()
+                    print(f"Kryziuku pergales: {pergales.win_x}\n"
+                          f"Nuliuku pergales: {pergales.win_o}\n"
+                          f"Lygiosios: {pergales.draw}")
                     break
                 objektas.game_window()
                 if turn % 2 == 0:
@@ -79,6 +109,12 @@ class BoardNum:
                 else:
                     objektas.turn_o()
 
+class WinCounter:
+    def __init__(self, win_x = 0, win_o = 0, draw= 0):
+        self.win_x = win_x
+        self.win_o = win_o
+        self.draw = draw
 
+pergales = WinCounter()
 objektas = BoardNum()
 objektas.game_state()
